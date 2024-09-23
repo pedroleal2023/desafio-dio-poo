65ed284768a1565ce1d664d639a5b5aaa1217c65
Pedro Miguel <rudaprod.oficial@gmail.com>
2024-09-23 20:48:12 -0300

    Implementing Methods.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.desafio.dominio.Conteudo;
-import br.com.desafio.dominio.Curso;
-import br.com.desafio.dominio.Mentoria;
+import br.com.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -22,9 +20,41 @@ public class Main {
         mentoria.setDescricao("Descrição: mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java");
+        bootcamp.setDescricao("Descrição: Bootcamp Java Developer.");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devPedro = new Dev();
+        devPedro.setNome("Pedro");
+        devPedro.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos: " + devPedro.getConteudosInscritos());
+
+        devPedro.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos de Pedro: " + devPedro.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos de Pedro: " + devPedro.getConteudosConcluido());
+        System.out.println("XP: " + devPedro.calcularTotalXp());
+
+        System.out.println("---------");
+
+        Dev devMatue = new Dev();
+        devMatue.setNome("Matue");
+        devMatue.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos: " + devMatue.getConteudosInscritos());
+        devMatue.progredir();
+        devMatue.progredir();
+        devMatue.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos de Matue: " + devMatue.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos de Matue: " + devPedro.getConteudosConcluido());
+        System.out.println("XP: " + devMatue.calcularTotalXp());
 
     }
 }
